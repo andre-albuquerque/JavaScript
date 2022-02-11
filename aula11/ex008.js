f48790cd798783aa75a011e3bf3a27c76ce3aaf5
andre-albuquerque <andrealbuquerqueleo@gmail.com>
2022-02-11 11:50:14 -0300

    initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade é ${vel} km/h`)
+if (vel > 60) { //condição simples
+    console.log('Ultrapassou a velocidade permitida.')
+}
+console.log('Dirija com cuidado!')
